c5bcaf60a489a10241c201782ecc35e82fb533f1
Loromhir <mosqueralorenzo@hotmail.com>
2022-08-18 19:25:48 -0300

    terminando el componente About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div></div>
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1>About Us</h1>
+            <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet debitis possimus architecto. 
+            Non totam voluptatem ducimus eaque tempore nulla suscipit illum dolore a fuga. Corporis aspernatur sunt labore 
+            doloribus quas?</p>
+            <NavLink to='/contact' className='btn btn-outline-primary'>Contact Us</NavLink>
+          </div>
+          <div className='col-md-6'>
+            <img src="/assets/images/about.jpg" alt="About Us" height='500px' width='500px' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

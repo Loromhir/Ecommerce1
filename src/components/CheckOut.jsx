2cd830ccbe7cde04b3f7baa1a9cc2f9c3848fec7
Loromhir <mosqueralorenzo@hotmail.com>
2022-08-27 17:29:00 -0300

    trabajo completado

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -11,35 +11,35 @@ const CheckOut = () => {
             <div className='row g-5'>
 
                 <div className='col-md-4 order-md-2 mb-4'>
-                    <h4 class="d-flex justify-content-between align-items-center mb-3">
-                        <span class="text-muted">Your cart</span>
-                        <span class="badge badge-secondary badge-pill">{state.length}</span>
+                    <h4 className="d-flex justify-content-between align-items-center mb-3">
+                        <span className="text-muted">Your cart</span>
+                        <span className="badge badge-secondary badge-pill">{state.length}</span>
                     </h4>
-                    <ul class="list-group mb-3">
+                    <ul className="list-group mb-3">
                        {state.map((item)=>{
                         total= total +item.price
                             return(
-                                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                                <li className="list-group-item d-flex justify-content-between lh-condensed">
                                 <div>
-                                    <h6 class="my-0">{item.title}</h6>
+                                    <h6 className="my-0">{item.title}</h6>
                                    
                                 </div>
-                                <span class="text-muted">${item.price}</span>
+                                <span className="text-muted">${item.price}</span>
                             </li>
                             )
                        })}
                        
-                        <li class="list-group-item d-flex justify-content-between">
+                        <li className="list-group-item d-flex justify-content-between">
                             <span>Total (USD)</span>
                             <strong>${total}</strong>
                         </li>
                     </ul>
 
-                    <form class="card p-2">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Promo code" />
-                            <div class="input-group-append">
-                                <button type="submit" class="btn btn-secondary">Redeem</button>
+                    <form className="card p-2">
+                        <div className="input-group">
+                            <input type="text" className="form-control" placeholder="Promo code" />
+                            <div className="input-group-append">
+                                <button type="submit" className="btn btn-secondary">Redeem</button>
                             </div>
                         </div>
                     </form>
@@ -51,14 +51,14 @@ const CheckOut = () => {
                         <div className="row">
                             <div className="col-md-6 mb-3">
                                 <label htmlFor="firstName">Primer nombre</label>
-                                <input type="text" className="form-control" id="firstName" placeholder="" value="" required="" />
+                                <input type="text" className="form-control" id="firstName" placeholder="" required="" />
                                 <div className="invalid-feedback">
                                     Se requiere validar el primer nombre.
                                 </div>
                             </div>
                             <div className="col-md-6 mb-3">
                                 <label htmlFor="lastName">Apellido</label>
-                                <input type="text" className="form-control" id="lastName" placeholder="" value="" required="" />
+                                <input type="text" className="form-control" id="lastName" placeholder="" required="" />
                                 <div className="invalid-feedback">
                                     Se requiere validar el apellido.
                                 </div>
@@ -103,7 +103,7 @@ const CheckOut = () => {
                             <div className="col-md-5 mb-3">
                                 <label htmlFor="country">Pais</label>
                                 <select className="custom-select d-block w-100" id="country" required="">
-                                    <option value="">Elije...</option>
+                                    <option >Elije...</option>
                                     <option>United States</option>
                                 </select>
                                 <div className="invalid-feedback">
@@ -143,7 +143,7 @@ const CheckOut = () => {
 
                         <div className="d-block my-3">
                             <div className="custom-control custom-radio">
-                                <input id="credit" name="paymentMethod" type="radio" className="custom-control-input" checked="" required="" />
+                                <input id="credit" name="paymentMethod" type="radio" className="custom-control-input" required="" />
                                 <label className="custom-control-label" htmlFor="credit"> Tarjeta de crédito</label>
                             </div>
                             <div className="custom-control custom-radio">
